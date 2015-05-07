86477e9003ab3bf54d90db39d7383bf3ebf400f6
joris <joris.lambrechts@bib.kuleuven.be>
2015-05-07 16:37:20 +0200

    design

--- admin/themes/default/common/footer.php
+++ admin/themes/default/common/footer.php
@@ -1,7 +1,7 @@
     </div>
-</div>
-    <footer class="" role="contentinfo">
-        <div class="container">
+
+    <footer class="container" role="contentinfo">
+        <div class="ten columns offset-by-two">
         <?php fire_plugin_hook('admin_footer', array('view'=>$this)); ?>
         <p class="left"><a href="http://www.omeka.org" target="_blank"><?php echo __('Powered by Omeka') ?></a> | <a href="http://omeka.org/codex" target="_blank"><?php echo __('Documentation'); ?></a> | <a href="http://omeka.org/forums/" target="_blank"><?php echo __('Support Forums'); ?></a></p>
         
@@ -11,7 +11,7 @@
         <?php endif; ?>
         </div>
         
-        <div id="footer_images" class="container">
+        <div id="footer_images" >
             <img src="<?php echo img("logo/EuropeanaSpace.png");?>">
             <img src="<?php echo img("logo/eu_logo-150x120.png");?>">
             <img src="<?php echo img("logo/spa_web_europeana.png");?>">
@@ -20,7 +20,7 @@
             <img src="<?php echo img("logo/KULEUVEN_CMYK_LOGO.PNG");?>">            
         </div>
     </footer>
-
+</div>
 <script type="text/javascript">
 jQuery(document).ready(function () {
     Omeka.runReadyCallbacks();
