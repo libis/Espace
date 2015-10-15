d515586dae1d1bb3b04e9ceb2c16b8f042aa00fe
joris <joris.lambrechts@bib.kuleuven.be>
2015-10-15 13:56:57 +0200

    fix #34

--- themes/espace/index.php
+++ themes/espace/index.php
@@ -9,7 +9,7 @@
                 <p><span class='image-overlay-text'><?php echo $homepageText; ?></span></p>
                 <?php endif; ?>
             </div>
-            <h2 id="get_started">&#10146;   Get Started Now</h2>
+            <h2 id="get_started"><a href="<?php echo url('create-your-own-story');?>">&#10146;   Get Started Now</a></h2>
         </div>
 
         <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
@@ -46,7 +46,7 @@
         <div class="gutter-sizer"></div>
         
         <div class="stamp story featured popular new">
-            <p><img class="make-icon" src="<?php echo img('book-icon_white.png');?>"><br>Create your own story</p>
+            <p><a href='<?php echo url('create-your-own-story');?>'><img class="make-icon" src="<?php echo img('book-icon_white.png');?>"><br>Create your own story</a></p>
         </div>
         
         <?php if(is_array($stories)):?>
@@ -79,95 +79,7 @@
             <?php echo $stories;?>
         <?php endif;?>
             
-        <div class="story featured">            
-            <div class="images"><img src="http://www.photoconsortium.net/wp-content/uploads/2015/01/R8_M_742_103_MHF-474x284.jpg"></div>
-            <div class="words">
-                <h3>Maximus mi diam</h3>
-                <p>Suspendisse maximus mi diam, at posuere lacus dictum quis. Quisque consectetur orci felis, eu pretium lacus ornare eu. Curabitur non augue at ipsum lacinia lobortis sit amet ornare lorem.</p>
-            <div class="more">
-                    <a href="">read more</a>
-                </div>
-            </div>
-        </div>
         
-        <div class="story">
-            
-           
-            <div class="images"><img src="http://sgdap.girona.cat/sdam/imatges/079240.jpg"></div>
-            <div class="words">
-                <h3>Maximus mi diam</h3>
-                <p>Suspendisse maximus mi diam, at posuere lacus dictum quis. Quisque consectetur orci felis, eu pretium lacus ornare eu. Curabitur non augue at ipsum lacinia lobortis sit amet ornare lorem.</p>
-            <div class="more">
-                    <a href="">read more</a>
-                </div>
-            </div>
-        </div>
-        
-        <div class="story">              
-            <div class="images"><img src="http://resolver.lias.be/get_pid?stream&usagetype=THUMBNAIL&pid=849766"></div>
-            <div class="words">
-                <h3>Maximus mi diam</h3>
-                <p>Suspendisse maximus mi diam, at posuere lacus dictum quis. Quisque consectetur orci felis, eu pretium lacus ornare eu. Curabitur non augue at ipsum lacinia lobortis sit amet ornare lorem.</p>
-            <div class="more">
-                    <a href="">read more</a>
-                </div>
-            </div>
-        </div>
-        
-        <div class="story featured">               
-            <div class="images"><img src="http://www.musei.uniroma1.it/dbinfo/RMSMUS06/JPEG/V.O.114.jpg"></div>
-            <div class="words">
-                <h3>Maximus mi diam</h3>
-                <p>Suspendisse maximus mi diam, at posuere lacus dictum quis. Quisque consectetur orci felis, eu pretium lacus ornare eu. Curabitur non augue at ipsum lacinia lobortis sit amet ornare lorem.</p>
-            <div class="more">
-                    <a href="">read more</a>
-                </div>
-            </div>
-        </div>
-        
-        <div class="story new popular">               
-            <div class="images"><img src="http://europeana.parisiennedephotographie.fr/SSEU/Files/8580ab52844c0d0a8d2d5935d639615d5ad8607ff2e97be1d5b145ebbf6ff037"></div>
-            <div class="words">
-                <h3>Maximus mi diam</h3>
-                <p>Suspendisse maximus mi diam, at posuere lacus dictum quis. Quisque consectetur orci felis, eu pretium lacus ornare eu. Curabitur non augue at ipsum lacinia lobortis sit amet ornare lorem.</p>
-            <div class="more">
-                    <a href="">read more</a>
-                </div>
-            </div>
-        </div>
-        
-        <div class="story popular">               
-            <div class="images"><img src="http://www.photoconsortium.net/wp-content/uploads/2015/01/Digital-Gallery.-Promoter-srl-843-694x416.jpg"></div>
-            <div class="words">
-                <h3>Maximus mi diam</h3>
-                <p>Suspendisse maximus mi diam, at posuere lacus dictum quis. Quisque consectetur orci felis, eu pretium lacus ornare eu. Curabitur non augue at ipsum lacinia lobortis sit amet ornare lorem.</p>
-            <div class="more">
-                    <a href="">read more</a>
-                </div>
-            </div>
-        </div>
-        
-        <div class="story">               
-            <div class="images"><img src="http://sgdap.girona.cat/sdam/imatges/066731.jpg"></div>
-            <div class="words">
-                <h3>Maximus mi diam</h3>
-                <p>Suspendisse maximus mi diam, at posuere lacus dictum quis. Quisque consectetur orci felis, eu pretium lacus ornare eu. Curabitur non augue at ipsum lacinia lobortis sit amet ornare lorem.</p>
-            <div class="more">
-                    <a href="">read more</a>
-                </div>
-            </div>
-        </div>
-        
-        <div class="story">               
-            <div class="images"><img src="http://sgdap.girona.cat/sdam/imatges/066579.jpg"></div>
-            <div class="words">
-                <h3>Maximus mi diam</h3>
-                <p>Suspendisse maximus mi diam, at posuere lacus dictum quis. Quisque consectetur orci felis, eu pretium lacus ornare eu. Curabitur non augue at ipsum lacinia lobortis sit amet ornare lorem.</p>
-                <div class="more">
-                    <a href="">read more</a>
-                </div>
-            </div>            
-        </div>
         
 </div>
 
--- themes/espace/simple-pages/page/show.php
+++ themes/espace/simple-pages/page/show.php
@@ -14,21 +14,18 @@ echo head(array(
     <p id="simple-pages-breadcrumbs"><?php echo simple_pages_display_breadcrumbs(); ?></p>
     <h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
     <?php endif; ?>
-<div id="primary">
-    
+<div id="primary">    
     <?php
     $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
     echo $this->shortcodes($text);
     ?>
 </div>
 <div id='secondary'>
-    
+    <?php echo simple_pages_navigation();?>
+    <?php if(metadata('simple_pages_page', 'title')!='Create your own story'):?>
     <div class="stamp">
-        <p>
-            <img class="make-icon" src="http://localhost/omeka/espace/themes/espace/images/book-icon_white.png">
-            <br>
-            Create your own story
-            </p>
+        <p><a href='<?php echo url('create-your-own-story');?>'><img class="make-icon" src="<?php echo img('book-icon_white.png');?>"><br>Create your own story</a></p>
     </div>
+    <?php endif;?>
 </div>
 <?php echo foot(); ?>
