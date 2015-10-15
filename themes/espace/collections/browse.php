5ddea896818ec83c5fbb920195787924f969181b
joris <joris.lambrechts@bib.kuleuven.be>
2015-10-15 16:05:52 +0200

    fix #31

--- themes/espace/collections/browse.php
+++ themes/espace/collections/browse.php
@@ -4,6 +4,7 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'collections browse'));
 ?>
 
 <h1><?php echo $pageTitle; ?> <?php echo __('(%s total)', $total_results); ?></h1>
+<?php echo search_form(); ?>
 <?php echo pagination_links(); ?>
 
 <?php
