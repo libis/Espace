2fb757b147364d3d8dce4130c01cabe6a29071bd
Naeem <naeem.muhammad@libis.kuleuven.be>
2015-12-16 17:09:19 +0100

    correction of home page link on login form

--- themes/espace/users/login.php
+++ themes/espace/users/login.php
@@ -6,7 +6,7 @@ echo head(array('bodyclass' => 'login', 'title' => $pageTitle), $header);
 <h1><?php echo $pageTitle; ?></h1>
 
 <p id="login-links">
-<span id="backtosite"><?php echo link_to_home_page(__('Go to Home Page')); ?></span>  |  <span id="forgotpassword"><?php echo link_to('users', 'forgot-password', __('Lost your password?')); ?></span>
+<span id="backtosite"><?php echo "<a href='".url("/")."'>" . __("Go to Home Page") . "</a>"; ?></span>  |  <span id="forgotpassword"><?php echo link_to('users', 'forgot-password', __('Lost your password?')); ?></span>
 </p>
 
 <?php echo flash(); ?>
